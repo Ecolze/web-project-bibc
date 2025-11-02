643598af4c22f0a64df23983eb3551180ccb1495
Ecolze <alfarizijambu54@gmail.com>
2025-11-02 17:08:48 +0700

    FIX: LOGO DAN TAMPILAN HP

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,23 @@
 const navbarNav = document.querySelector('.navbar-nav');
-const hamburgerMenu = document.querySelector('#Menu');
 
-hamburgerMenu.onclick = (e) => {
-
+document.querySelector('#Menu').onclick = (e) => {
   navbarNav.classList.toggle('active');
+  
+
+  document.body.classList.toggle('no-scroll');
+
+  
   e.preventDefault(); 
 };
 
-document.addEventListener('click', function(e) {
 
-  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+const hamburger = document.querySelector('#Menu');
+document.addEventListener('click', function(e) {
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+    
+
+    document.body.classList.remove('no-scroll');
+
   }
 });
